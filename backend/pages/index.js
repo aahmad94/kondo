083c76a645298f711007b73a76756bd833814601
aahmad94 <aka5vm@virginia.edu>
2024-10-01 23:47:24 -0700

    add index.js for base url

---
+++ backend/pages/index.js
@@ -0,0 +1,15 @@
+// pages/index.js
+import React from 'react';
+import Dashboard from './dashboard';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Dashboard />
+      {/* Add more content or components here */}
+    </div>
+  );
+}
+
+export default HomePage;
