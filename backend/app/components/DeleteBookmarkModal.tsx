628ff09360fdfd58822023ab4f967ef4c1fc089e
aahmad94 <aka5vm@virginia.edu>
2024-10-26 22:26:53 -0700

    fix quotes to math eslint escape rules

--- backend/app/components/DeleteBookmarkModal.tsx
+++ backend/app/components/DeleteBookmarkModal.tsx
@@ -14,7 +14,7 @@ const DeleteBookmarkModal: React.FC<DeleteBookmarkModalProps> = ({ isOpen, onClo
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="bg-gray-800 text-white p-6 rounded-lg">
         <h2 className="text-xl font-bold mb-4">Delete Bookmark</h2>
-        <p className="mb-4">Are you sure you want to delete the bookmark "{bookmarkTitle}" and all its contents?</p>
+        <p className="mb-4">Are you sure you want to delete the bookmark &quot;{bookmarkTitle}&quot; and all its contents?</p>
         <div className="flex justify-end">
           <button
             className="px-4 py-2 bg-gray-600 text-white rounded mr-2 hover:bg-gray-700"
