1bda48c1fa7d339f6d0cdeed5194d1e552be92e1
aahmad94 <aka5vm@virginia.edu>
2025-02-24 11:55:47 -0800

    dev: change api to gpt-4o

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -26,7 +26,7 @@ export default async function handler(
     const systemPrompt = getPromptFromFile(languageCode);
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-4-turbo-preview",
+      model: "gpt-4o",
       messages: [
         { role: "system", content: systemPrompt },
         { role: "user", content: prompt }
