f3c5b76cf83fbb928a1b4933bbc53a04274fe654
aahmad94 <aka5vm@virginia.edu>
2025-05-18 11:12:36 -0400

    feat: breakdown modal has a sticky header w/ updated button styling

--- app/components/BreakdownModal.tsx
+++ app/components/BreakdownModal.tsx
@@ -14,14 +14,14 @@ const BreakdownModal: React.FC<BreakdownModalProps> = ({ isOpen, onClose, breakd
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-[#111111] p-6 rounded-sm w-[600px] max-w-[90vw] max-h-[80vh] overflow-y-auto">
-        <div className="flex justify-between items-center mb-4">
+      <div className="bg-[#111111] p-6 rounded-sm w-[600px] max-w-[90vw] max-h-[80vh] flex flex-col">
+        <div className="flex justify-between items-center sticky top-0 bg-[#111111] pb-4">
           <h2 className="text-l font-bold" style={{ color: '#b59f3b' }}>Breakdown</h2>
-          <button onClick={onClose} className="text-white">
+          <button onClick={onClose} className="text-white hover:opacity-70 transition-opacity duration-200">
             <XMarkIcon className="h-6 w-6" />
           </button>
         </div>
-        <div className="text-[#b59f3b] whitespace-pre-wrap">
+        <div className="text-[#b59f3b] whitespace-pre-wrap overflow-y-auto">
           <Markdown remarkPlugins={[remarkGfm]} className="overflow-hidden">
             {breakdown}
           </Markdown>
